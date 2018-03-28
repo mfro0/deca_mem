f0f4c7953818552b5bce38b0e452e87b48c89410
Markus Fröschle <mfro@mubf.de>
2018-03-28 19:49:03 +0200

    add missing reset_button.vhd

---
+++ reset_button.vhd
@@ -0,0 +1,22 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity reset_button is
+    port
+    (
+        clk             : in std_ulogic;
+        button          : in std_ulogic;
+        reset_out_n     : out std_ulogic
+    );
+end entity reset_button;
+        
+architecture rtl of reset_button is
+    signal sync_button  : std_ulogic_vector(1 downto 0);
+begin
+    p_sync_button : process
+    begin
+        wait until rising_edge(clk);
+        sync_button <= sync_button(0) & button;
+        reset_out_n <= not sync_button(1);
+    end process p_sync_button;
+end architecture rtl;
