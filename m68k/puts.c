8c1ac921ae8874dc4ccfbd5a21c7d421b626e62e
Markus Fröschle <mfro@mubf.de>
2018-04-02 18:47:01 +0200

    add simple m68k test code

---
+++ m68k/puts.c
@@ -0,0 +1,20 @@
+static inline int putc(int c)
+{
+    volatile int *term_ready = (volatile int *) 0xfffffff0;
+    volatile int *term_out = (volatile int *) 0xfffffff4;
+
+    while (!*term_ready);
+    *term_out = c;
+
+    return 1;
+}
+
+int puts(char *str)
+{
+    int c;
+
+    while (c = *str++)
+        putc(c);
+
+    return 0;
+}
