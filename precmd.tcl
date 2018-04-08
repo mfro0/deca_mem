0018d31b97d70ac5ec686cb79370802ab61b5fd6
Markus Fröschle <mfro@mubf.de>
2018-04-08 09:53:15 +0200

    add comment

--- precmd.tcl
+++ precmd.tcl
@@ -1,3 +1,9 @@
+#
+# execute each of a list of tcl scripts
+#
+# meant to be used with the PRE_FLOW_SCRIPT_FILE quartus assignment
+# that allows to evaluate a tcl script before analysis starts
+
 set precmd_list { "embed_m68k.tcl" "make_datetime.tcl" }
 
 foreach item $precmd_list {
